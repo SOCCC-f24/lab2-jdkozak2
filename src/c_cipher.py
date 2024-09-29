2d328017fa578978cb85aff3159ab9f050e063a4
jdkozak2 <jdk22@sunyorange.edu>
2024-09-29 16:59:24 -0400

    Update c_cipher.py

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -1,16 +1,12 @@
 import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-def encrypt(email="abc012"):
-    """
-    TODO: What is the objective? 
+ # keep all updates in the retVal (str) variablei
+    # i.e.,
+    #    email_str = " some string updates here "
+    #    email_1 = email_str.strip()
+    #    retVal = email_1def encrypt(email="abc012"):
 
-    Args:
-        TODO: what arguments and data types are expected? (i.e., email)
-
-    Returns:
-        TODO: what varibale and data types are being returned?   
-    """
     output = "" 
     len_flag = len(email) != 6
     # TODO: fix line below and, implement functionality rather than literals
@@ -19,7 +15,9 @@ def encrypt(email="abc012"):
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+    A = email[:3]
+    B = email[:3]
+    anum_flag = A != 'abc' or B != '012' 
 
     if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
@@ -33,32 +31,36 @@ def encrypt(email="abc012"):
         return output     
         
     # TODO: fix line below, process our string into a list
-    email_lst = ["a", "b", "c", "0", "1", "2"]
+    email = "abc012"
+    email_lst = list(email)
+    print(email_lst)
         
     # TODO: complete line(s) below, convert EACH new element into a string
-    new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
-    email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+    for i in range(len(email_lst)):
+    new_ascii = ord(email_lst[i]) + 3    # NOTE: here we extract and update element at 0 
+    email_lst[i] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+
+    print(email_lst)
         
     # TODO: fix line below, convert list into a string
-    email_str = "dbc012"
+   email_lst = ['d', 'b', 'c', '0', '1', '2'] 
+    email_str = ''.join(email_lst)
+    print(email_str)
 
-    # keep all updates in the retVal (str) variablei
-    # i.e.,
-    #    email_str = " some string updates here "
-    #    email_1 = email_str.strip()
-    #    retVal = email_1
+   
     retVal = email_str
     return retVal 
 
 def decrypt(email="def345"):
     """
     TODO: What is the objective? 
-
+    The objective of this line is to reverse the encrytion process applied to email string. 
     Args:
         TODO: what arguments and data types are expected? (i.e., email)
-
+        The decrypt function is going to use a single line argument with the name/data type being "email" which will be a string.  
     Returns:
-        TODO: what varibale and data types are being returned?   
+        TODO: what varibale and data types are being returned?  
+        A string, bool, and int data types. 
     """
     # input validation
     output = "" 
@@ -69,7 +71,11 @@ def decrypt(email="def345"):
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    anum_flag = email[:3] != 'def' or email[3:] != '345' 
+    first = 'def'
+    second = '345'
+    A = email[:3]
+    B = email[:3]
+    anum_flag = A != first or B != second 
 
     if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
